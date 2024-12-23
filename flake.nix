ab159a498b7e20854cd1a4af48b14daf100ec0b3
Ryan Lynch <rynplynch@gmail.com>
2024-12-23 15:24:34 -0800

    dotnet module attributes

--- flake.nix
+++ flake.nix
@@ -18,20 +18,21 @@
           inherit system;
         };
 
+        pname = "rplwork_client";
+        version = "0.0.1";
+        projectFile = "rplwork_client.csproj";
+        src = ./src;
+
         dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0_1xx;
         dotnet-runtime = pkgs.dotnetCorePackages.aspnetcore_8_0;
-        version = "0.0.1";
-          projectFile = "rplwork.csproj";
-                src = ./src;
       in rec {
         apps.default = flake-utils.lib.mkApp {
           drv = self.packages.${system}.default;
         };
 
         packages.rplwork = pkgs.buildDotnetModule {
-                    inherit src projectFile dotnet-sdk dotnet-runtime version;
-          pname = "rplwork_client";
           nugetDeps = ./deps.nix; #`nix build .#default.passthru.fetch-deps && ./result` and put the result here
+          inherit pname src projectFile dotnet-sdk dotnet-runtime version;
           doCheck = true;
         };
 
