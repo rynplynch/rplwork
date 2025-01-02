bd78a75ae5d36e5a6bbcb88f575c07d36c26a927
Ryan Lynch <rynplynch@gmail.com>
2025-01-02 00:10:09 -0800

    helps us manage builds for other systems

--- flake.nix
+++ flake.nix
@@ -18,6 +18,8 @@
   outputs = inputs @ {flake-parts, ...}:
   # generate the flake, while passing input attributes
     flake-parts.lib.mkFlake {inherit inputs;} {
+      # different systems that rplwork can be built for
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
 
         pname = "rplwork_client";
         version = "0.0.1";
@@ -26,6 +28,13 @@
 
         dotnet-sdk = pkgs.dotnetCorePackages.sdk_8_0_1xx;
         dotnet-runtime = pkgs.dotnetCorePackages.aspnetcore_8_0;
+      # helper function that handles ${system} for us
+      perSystem = {
+        # used to reference nixpkgs, called because we inherited inputs
+        pkgs,
+        self',
+        ...
+      }: {
         };
 
         packages.default = packages.rplwork;
@@ -61,4 +70,5 @@
         };
       }
     );
+    };
 }
