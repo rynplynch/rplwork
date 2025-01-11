06c992348ec3b02230da16635f90d56e62ab6ede
Ryan Lynch <rynplynch@gmail.com>
2025-01-10 18:15:32 -0800

    Use pname attribute instead of hard coded value

--- pkgs/rplwork_client_image.nix
+++ pkgs/rplwork_client_image.nix
@@ -8,18 +8,20 @@
   image = dockerTools.buildImage {
     name = lib.strings.concatStrings [rplwork_client.pname "_" rplwork_client.version];
     tag = "latest";
+
     copyToRoot = buildEnv {
       name = "image-root";
       paths = [rplwork_client];
       pathsToLink = ["/bin"];
     };
+
     runAsRoot = ''
       #!${runtimeShell}
       mkdir -p /tmp
     '';
 
     config = {
-      Cmd = ["/bin/rplwork_client"];
+      Cmd = [rplwork_client.pname];
 
       ExposedPorts = {
         "5000" = {};
