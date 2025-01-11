4807b8b2d25f89230be337d104746c4bc02c94a1
Ryan Lynch <rynplynch@gmail.com>
2025-01-10 18:17:51 -0800

    Create port attribute and add to derivation

--- pkgs/rplwork_client.nix
+++ pkgs/rplwork_client.nix
@@ -8,14 +8,14 @@
   version = "1.0.0";
   projectFile = "rplwork_client.csproj";
   src = ../src;
-  port = "5001";
+  port = "5000";
 
   dotnet-sdk = dotnetCorePackages.sdk_8_0_1xx;
   dotnet-runtime = dotnetCorePackages.aspnetcore_8_0;
 
   nuget-packageslock2nix = inputs.nuget-packageslock2nix;
   rplwork = buildDotnetModule {
-    inherit pname src projectFile dotnet-sdk dotnet-runtime version;
+    inherit pname src projectFile dotnet-sdk dotnet-runtime version port;
 
     nugetDeps = nuget-packageslock2nix.lib {
       inherit system;
