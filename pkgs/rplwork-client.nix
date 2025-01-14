ec6dfc82a4014930c04fa99b7cf98b8755bb9228
Ryan Lynch <rynplynch@gmail.com>
2025-01-13 16:34:58 -0800

    Patch bump, correctly serves static css files

--- pkgs/rplwork-client.nix
+++ pkgs/rplwork-client.nix
@@ -6,7 +6,7 @@
 }: let
   # started configuration attributes for dotnet projects
   pname = "rplwork_client";
-  version = "1.0.0";
+  version = "1.0.1";
   projectFile = "rplwork_client.csproj";
   src = ../rplwork_client;
   port = "5000";
