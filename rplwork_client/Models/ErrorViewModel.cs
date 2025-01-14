096bef38cc9f2d3987879d4597d8ad68ae6cdc2d
Ryan Lynch <rynplynch@gmail.com>
2025-01-14 13:22:53 -0800

    Moved to ViewModels, added ProjectViewModel

--- rplwork_client/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace rplwork_client.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
